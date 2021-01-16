8a946c65e46512497948f90f72f78ade8e5cde54
Alexsandro Leão Lima <sandro.lima@viatek.local>
2021-01-16 00:00:28 -0300

    Database seeding

--- backend/src/main/java/com/example/services/CategoryService.java
+++ backend/src/main/java/com/example/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.entities.Category;
 import com.example.repositories.CategoryRespository;
@@ -14,6 +15,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRespository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return repository.findAll();
 	}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into tb_category (name) values ('Books');
+
+insert into tb_category (name) values ('Electronics');
+
+insert into tb_category (name) values ('Computers');
