82cb4838419cd9bc49278a7d11734d198595a165
Sandro Lima <sandro.lima0016@gmail.com>
2021-02-08 10:50:27 -0300

    Update

--- front-web/src/core/components/ButtonIcon/index.tsx
+++ front-web/src/core/components/ButtonIcon/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 const ButtonIcon = ({ text }: Props) => (
     <div className="d-flex">
         <button className="btn btn-primary btn-icon">
-        <h5>{text}</h5>
+            <h5>{text}</h5>
         </button>
         <div className="btn-icon-content">
             <ArrowIcon />
