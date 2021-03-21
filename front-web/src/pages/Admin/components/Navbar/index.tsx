350cffcc5a336aa954ec1c55ca43ceb94552e4df
Alexsandro Leão Lima <sandro.lima@viatek.local>
2021-03-21 01:30:00 -0300

    Add allowed roles on components

--- front-web/src/pages/Admin/components/Navbar/index.tsx
+++ front-web/src/pages/Admin/components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import { isAllowedByRole } from 'core/utils/auth';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './styles.scss';
@@ -16,11 +17,13 @@ const Navbar = () => (
                     Minhas Categorias
                 </NavLink>
             </li>
-            <li>
+            {isAllowedByRole(['ROLE_ADMIN']) && (
+                <li>
                 <NavLink to="/admin/users" className="admin-nav-item">
                     Meus Usuários
                 </NavLink>
             </li>
+            )}
         </ul>
     </nav>
 );
