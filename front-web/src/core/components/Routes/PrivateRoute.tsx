7a6bf27fe18b4abf235bb493ca00b0268ff6d2f1
Alexsandro Leão Lima <sandro.lima@viatek.local>
2021-03-21 01:18:03 -0300

    Add allowed roles on private route

---
+++ front-web/src/core/components/Routes/PrivateRoute.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router';
+import { isAllowedByRole, isAuthenticated, Role } from 'core/utils/auth';
+
+type Props = {
+    children: React.ReactNode;
+    path: string;
+    allowedRoutes?: Role[];
+}
+
+const PrivateRoute = ({ children, path, allowedRoutes }: Props) => {
+    return (
+        <Route
+            path={path}
+            render={({ location }) => {
+                if (!isAuthenticated()) {
+                    return (
+                        <Redirect
+                            to={{
+                                pathname: "/admin/auth/login",
+                                state: { from: location }
+                            }}
+                        />
+                    )
+                } else if (isAuthenticated() && !isAllowedByRole(allowedRoutes)) {
+                    return (
+                        <Redirect to={{ pathname: "/admin" }} />
+                    )
+                }
+
+                return children;
+            }}
+        />
+    );
+}
+
+export default PrivateRoute;
--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,6 +1,6 @@
 import ButtonIcon from 'core/components/ButtonIcon';
 import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss';
@@ -12,17 +12,25 @@ type FormData = {
     password: string;
 }
 
+type LocationState = {
+    from: string;
+}
+
 const Login = () => {
     const { register, handleSubmit, errors } = useForm<FormData>();
     const [hasError, setHasError] = useState(false);
     const history = useHistory();
+    const location = useLocation<LocationState>();
+
+    const { from } = location.state || { from: {pathame:"/admin"} };
 
     const onSubmit = (data: FormData) => {
         makeLogin(data)
             .then(response => {
                 setHasError(false);
                 saveSessionData(response.data);
-                history.push('/admin');
+                history.replace(from);
+
             })
             .catch(() => {
                 setHasError(true);
