9d7969041e2dac3a93cb3ea1ccf2ac3d4db84af3
Sandro Lima <sandro.lima0016@gmail.com>
2021-03-23 11:21:55 -0300

    Intregate product listing with backend

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import ProductPrice from 'core/components/ProductPrice';
 import './styles.scss';
+import { Product } from 'core/types/Product';
 
-const Card = () => {
+type Props = {
+  product: Product;
+}
+
+const Card = ({ product }: Props) => {
   return (
     <div className="card-base product-card-admin">
       <div className="row">
         <div className="col-2 text-center border-right py-3">
           <img
-            src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"
-            alt="Produto teste"
+            src={product.imgUrl}
+            alt={product.name}
             className="product-card-image-admin"
           />
         </div>
         <div className="col-7 py-3">
           <h3 className="product-card-name-admin">
-            Computador i7
+            {product.name}
           </h3>
-          <ProductPrice price={40.5} />
+          <ProductPrice price={product.price} />
           <div>
-            <span className="badge rounded-pill bg-secondary mr-2">Categoria 1</span>
-            <span className="badge rounded-pill bg-secondary mr-2">Categoria 2</span>
-            <span className="badge rounded-pill bg-secondary mr-2">Categoria 3</span>
+            <span className="badge rounded-pill bg-secondary mr-2">
+              Categoria 1
+            </span>
+            <span className="badge rounded-pill bg-secondary mr-2">
+              Categoria 2
+            </span>
           </div>
         </div>
         <div className="col-3 pt-3 pr-5">
--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
 import { useHistory } from 'react-router-dom';
 import Card from '../Card';
+import Pagination from 'core/components/Pagination';
 
 const List = () => {
+    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
     const history = useHistory();
+
+    console.log(productsResponse);
+
+
+    useEffect(() => {
+
+        const params = {
+            page: activePage,
+            linesPerPage: 4
+        }
+        setIsLoading(true);
+        makeRequest({ url: '/products', params })
+            .then(response => setProductsResponse(response.data))
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }, [activePage]);
 
     const handleCreate = () => {
         history.push('/admin/products/create')
@@ -15,9 +38,16 @@ const List = () => {
                 ADICIONAR
             </button>
             <div className="admin-list-container">
-                <Card />
-                <Card />
-                <Card />
+                {productsResponse?.content.map(product => (
+                    <Card product={product} key={product.id} />
+                ))}
+                {productsResponse && (
+                    <Pagination
+                        totalPages={productsResponse.totalPages}
+                        activePage={activePage}
+                        onChange={page => setActivePage(page)}
+                    />
+                )}
             </div>
         </div>
     );
