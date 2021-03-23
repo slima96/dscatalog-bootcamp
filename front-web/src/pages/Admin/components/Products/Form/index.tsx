ba16b5dfd1542468542c2c45efd94662e2911dc8
Sandro Lima <sandro.lima0016@gmail.com>
2021-03-23 15:33:52 -0300

    Edit product

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
-import { makePrivateRequest } from 'core/utils/request';
+import { makePrivateRequest, makeRequest } from 'core/utils/request';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
-import { useHistory } from 'react-router';
+import { useHistory, useParams } from 'react-router';
+import { title } from 'process';
 
 type FormState = {
     name: string;
     price: string;
     description: string;
-    imageUrl: string;
+    imgUrl: string;
+}
+
+type ParamsType = {
+    productId: string;
 }
 
 const Form = () => {
-    const { register, handleSubmit, errors } = useForm<FormState>();
+    const { register, handleSubmit, errors, setValue } = useForm<FormState>();
     const history = useHistory();
+    const { productId } = useParams<ParamsType>();
+    const isEditing = productId !== 'create';
+    const formTitle = isEditing ? 'Editar Produto' : 'cadastrar um produto';
+
+    useEffect(() => {
+        if (isEditing) {
+            makeRequest({ url: `/products/${productId}` })
+                .then(response => {
+                    setValue('name', response.data.name);
+                    setValue('price', response.data.price);
+                    setValue('description', response.data.description);
+                    setValue('imgUrl', response.data.imgUrl);
+                })
+        }
+    }, [productId, isEditing, setValue]);
 
     const onSubmit = (data: FormState) => {
-        makePrivateRequest({ url: '/products', method: 'POST', data })
+        makePrivateRequest({
+            url: isEditing ? `/products/${productId}` : '/products',
+            method: isEditing ? 'PUT' : 'POST',
+            data
+        })
             .then(() => {
                 toast.info('Produto cadastrado com sucesso!');
                 history.push('/admin/products');
@@ -30,7 +54,9 @@ const Form = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <BaseForm title="cadastrar um produto">
+            <BaseForm
+                title={formTitle}
+            >
                 <div className="row">
                     <div className="col-6">
                         <div className="margin-bottom-30">
@@ -69,14 +95,14 @@ const Form = () => {
                             <div className="margin-bottom-30">
                                 <input
                                     ref={register({ required: "Campo obrigatório" })}
-                                    name="imageUrl"
+                                    name="imgUrl"
                                     type="text"
                                     className="form-control input-base"
                                     placeholder="Imagem do produto"
                                 />
-                                {errors.imageUrl && (
+                                {errors.imgUrl && (
                                     <div className="invalid-feedback d-block">
-                                        {errors.imageUrl.message}
+                                        {errors.imgUrl.message}
                                     </div>
                                 )}
                             </div>
--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -11,14 +11,13 @@ const List = () => {
     const [activePage, setActivePage] = useState(0);
     const history = useHistory();
 
-    console.log(productsResponse);
-
-
     useEffect(() => {
 
         const params = {
             page: activePage,
-            linesPerPage: 4
+            linesPerPage: 4,
+            direction: 'DESC',
+            orderBy: 'id'
         }
         setIsLoading(true);
         makeRequest({ url: '/products', params })
