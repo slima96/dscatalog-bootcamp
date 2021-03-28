39ecc650e0eb2cc2f66a8e28d82a3d36fa2f0ddc
Alexsandro Leão Lima <sandro.lima@viatek.local>
2021-03-28 00:47:21 -0300

    Add product image upload

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -8,156 +8,161 @@ import BaseForm from '../../BaseForm';
 import { Category } from 'core/types/Product';
 import './styles.scss';
 import PriceField from './PriceField';
+import ImageUpload from '../ImageUpload';
 
 export type FormState = {
-  name: string;
-  price: string;
-  description: string;
-  imgUrl: string;
-  categories: Category[];
+    name: string;
+    price: string;
+    description: string;
+    imgUrl: string;
+    categories: Category[];
 }
 
 type ParamsType = {
-  productId: string;
+    productId: string;
 }
 
 const Form = () => {
-  const { register, handleSubmit, errors, setValue, control } = useForm<FormState>();
-  const history = useHistory();
-  const { productId } = useParams<ParamsType>();
-  const [isLoadingCategories, setIsLoadingCategories] = useState(false);
-  const [categories, setCategories] = useState<Category>();
-  const isEditing = productId !== 'create';
-  const formTitle = isEditing ? 'Editar Produto' : 'cadastrar um produto'; 
+    const { register, handleSubmit, errors, setValue, control } = useForm<FormState>();
+    const history = useHistory();
+    const { productId } = useParams<ParamsType>();
+    const [isLoadingCategories, setIsLoadingCategories] = useState(false);
+    const [categories, setCategories] = useState<Category>();
+    const [uploadedImgUrl, setUploadedImgUrl] = useState(''); 
+    const [productImgUrl, setProductImgUrl] = useState('');
+    
+    const isEditing = productId !== 'create';
+    const formTitle = isEditing ? 'Editar Produto' : 'cadastrar um produto';
 
-  useEffect(() => {
-    if (isEditing) {
-      makeRequest({ url: `/products/${productId}` })
-        .then(response => {
-          setValue('name', response.data.name);
-          setValue('price', response.data.price);
-          setValue('description', response.data.description);
-          setValue('imgUrl', response.data.imgUrl);
-          setValue('categories', response.data.categories);
+    useEffect(() => {
+        if (isEditing) {
+            makeRequest({ url: `/products/${productId}` })
+                .then(response => {
+                    setValue('name', response.data.name);
+                    setValue('price', response.data.price);
+                    setValue('description', response.data.description);
+                    setValue('categories', response.data.categories);
+
+                    setProductImgUrl(response.data.imgUrl);
+                })
+        }
+    }, [productId, isEditing, setValue]);
+
+    useEffect(() => {
+        setIsLoadingCategories(true);
+        makeRequest({
+            url: '/categories'
         })
+            .then(response => setCategories(response.data.content))
+            .finally(() => setIsLoadingCategories(false));
+    }, []);
+
+    const onSubmit = (data: FormState) => {
+        const payload = {
+            ...data,
+            imgUrl: uploadedImgUrl
+        }
+
+        makePrivateRequest({
+            url: isEditing ? `/products/${productId}` : '/products',
+            method: isEditing ? 'PUT' : 'POST',
+            data: payload
+        })
+            .then(() => {
+                toast.info('Produto salvo com sucesso!');
+                history.push('/admin/products');
+            })
+            .catch(() => {
+                toast.error('Erro ao salvar produto!');
+            })
     }
-  }, [productId, isEditing, setValue]);
 
-  useEffect(() => {
-    setIsLoadingCategories(true);
-    makeRequest({
-      url: '/categories'})
-        .then(response => setCategories(response.data.content))
-        .finally(() => setIsLoadingCategories(false));
-  }, []);
+    const onUploadSuccess = (imgUrl: string) => {
+        setUploadedImgUrl(imgUrl);
+    }
 
-  const onSubmit = (data: FormState) => {
-    makePrivateRequest({
-      url: isEditing ? `/products/${productId}` : '/products',
-      method: isEditing ? 'PUT' : 'POST',
-      data
-    })
-      .then(() => {
-        toast.info('Produto salvo com sucesso!');
-        history.push('/admin/products');
-      })
-      .catch(() => {
-        toast.error('Erro ao salvar produto!');
-      })
-  }
-
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <BaseForm
-        title={formTitle}
-      >
-        <div className="row">
-          <div className="col-6">
-            <div className="margin-bottom-30">
-              <input
-                ref={register({
-                  required: "Campo obrigatório",
-                  minLength: { value: 5, message: 'O campo deve ter no mínimo 5 caracteres' },
-                  maxLength: { value: 60, message: 'O campo deve ter no máximo 60 caracteres' }
-                })}
-                name="name"
-                type="text"
-                className="form-control input-base"
-                placeholder="Nome do produto"
-              />
-              {errors.name && (
-                <div className="invalid-feedback d-block">
-                  {errors.name.message}
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <BaseForm
+                title={formTitle}
+            >
+                <div className="row">
+                    <div className="col-6">
+                        <div className="margin-bottom-30">
+                            <input
+                                ref={register({
+                                    required: "Campo obrigatório",
+                                    minLength: { value: 5, message: 'O campo deve ter no mínimo 5 caracteres' },
+                                    maxLength: { value: 60, message: 'O campo deve ter no máximo 60 caracteres' }
+                                })}
+                                name="name"
+                                type="text"
+                                className="form-control input-base"
+                                placeholder="Nome do produto"
+                            />
+                            {errors.name && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.name.message}
+                                </div>
+                            )}
+                        </div>
+                        <div className="margin-bottom-30">
+                            <Controller
+                                as={Select}
+                                name="categories"
+                                rules={{ required: true }}
+                                control={control}
+                                isLoading={isLoadingCategories}
+                                options={categories}
+                                getOptionLabel={(option: Category) => option.name}
+                                getOptionValue={(option: Category) => String(option.id)}
+                                classNamePrefix="categories-select"
+                                placeholder="Categorias"
+                                inputId="categories"
+                                defaultValue=""
+                                isMulti
+                            />
+                            {errors.categories && (
+                                <div className="invalid-feedback d-block">
+                                    Campo obrigatório
+                                </div>
+                            )}
+                        </div>
+                        <div className="margin-bottom-30">
+                            <PriceField control={control} />
+                            {errors.price && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.price.message}
+                                </div>
+                            )}
+                        </div>
+                        <div className="margin-bottom-30">
+                            <ImageUpload 
+                                onUploadSuccess={onUploadSuccess}
+                                productImgUrl={productImgUrl}
+                            />
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <textarea
+                            ref={register({ required: "Campo obrigatório" })}
+                            name="description"
+                            className="form-control input-base"
+                            placeholder="Descrição"
+                            cols={30}
+                            rows={10}
+                        />
+                        {errors.description && (
+                            <div className="invalid-feedback d-block">
+                                {errors.description.message}
+                            </div>
+                        )}
+                    </div>
                 </div>
-              )}
-            </div>
-            <div className="margin-bottom-30">
-              <Controller
-                as={Select}
-                name="categories"
-                rules={{ required: true }}
-                control={control}
-                isLoading={isLoadingCategories}
-                options={categories}
-                getOptionLabel={(option: Category) => option.name}
-                getOptionValue={(option: Category) => String(option.id)}
-                classNamePrefix="categories-select"
-                placeholder="Categorias"
-                inputId="categories"
-                defaultValue=""
-                isMulti
-              />
-              {errors.categories && (
-                <div className="invalid-feedback d-block">
-                  Campo obrigatório
-                </div>
-              )}
-            </div>
-            <div className="margin-bottom-30">
-              <PriceField control={control}/>
-              {errors.price && (
-                <div className="invalid-feedback d-block">
-                  {errors.price.message}
-                </div>
-              )}
-            </div>
-            <div>
-              <div className="margin-bottom-30">
-                <input
-                  ref={register({ required: "Campo obrigatório" })}
-                  name="imgUrl"
-                  type="text"
-                  className="form-control input-base"
-                  placeholder="Imagem do produto"
-                />
-                {errors.imgUrl && (
-                  <div className="invalid-feedback d-block">
-                    {errors.imgUrl.message}
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="col-6">
-            <textarea
-              ref={register({ required: "Campo obrigatório" })}
-              name="description"
-              className="form-control input-base"
-              placeholder="Descrição"
-              cols={30}
-              rows={10}
-            />
-            {errors.description && (
-              <div className="invalid-feedback d-block">
-                {errors.description.message}
-              </div>
-            )}
-          </div>
-        </div>
-      </BaseForm>
-    </form>
+            </BaseForm>
+        </form>
 
-  );
+    );
 }
 
 export default Form;
---
+++ front-web/src/pages/Admin/components/Products/ImageUpload/index.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { ReactComponent as UploadPlaceholder } from 'core/assets/images/upload-placeholder.svg';
+import { makePrivateRequest } from 'core/utils/request';
+import { toast } from 'react-toastify';
+import './styles.scss';
+
+type Props = {
+  onUploadSuccess: (imgurl: string) => void;
+  productImgUrl: string;
+}
+
+const ImageUpload = ({ onUploadSuccess, productImgUrl }: Props) => {
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const [uploadedImgUrl, setUploadedImgUrl] = useState('');
+  const imgUrl = uploadedImgUrl || productImgUrl;
+
+  const onUploadProgress = (progressEvent: ProgressEvent) => {
+    const progress = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+
+    console.log(progress);
+
+    setUploadProgress(progress);
+  }
+
+  const uploadImage = (selectedImage: File) => {
+    const payload = new FormData();
+    payload.append('file', selectedImage);
+
+    makePrivateRequest({
+      url: 'products/image',
+      method: 'POST',
+      data: payload,
+      onUploadProgress
+    })
+      .then(response => {
+        setUploadedImgUrl(response.data.uri);
+        onUploadSuccess(response.data.uri);
+      })
+      .catch(() => {
+        toast.error("Erro ao enviar arquivo");
+      })
+      .finally(() => setUploadProgress(0));
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedImage = event.target.files?.[0];
+
+    if (selectedImage) {
+      uploadImage(selectedImage);
+    }
+
+  }
+
+  return (
+    <div className="row">
+      <div className="col-6">
+        <div className="upload-botton-container">
+          <input
+            type="file"
+            id="upload"
+            accept="image/png, image/jpg"
+            onChange={handleChange}
+            hidden
+          />
+          <label htmlFor="upload">ADICIONAR IMAGEM</label>
+        </div>
+        <small className="upload-text-hekper text-primary">
+          A imagem deve ser  JPG ou PNG e não deve ultrapassar <strong>5 mb.</strong>
+        </small>
+      </div>
+      <div className="col-6 upload-placeholder">
+        {uploadProgress > 0 && (
+          <>
+            <UploadPlaceholder />
+            <div className="upload-progress-container">
+              <div
+                className="upload-progress"
+                style={{ width: `${uploadProgress}%` }}>
+              </div>
+            </div>
+          </>
+        )}
+        {(imgUrl && uploadProgress === 0) && (
+          <img
+            src={imgUrl}
+            alt={imgUrl}
+            className="uploaded-image"
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default ImageUpload;
